07d17e089378857474c70eefe67ccf57c8503c26
mTL NARESH <mtlnaresh@mtl-naresh.local>
2018-10-11 18:37:50 +0530

    added sum function

--- sample/ViewController.swift
+++ sample/ViewController.swift
@@ -13,6 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("naresh")
+    }
+    func addNo() {
+        var a = 10,b = 12
+        var c = a+b
+        print(c)
     }
 
 
